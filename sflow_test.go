81a7cd3c679fe4b4b2d2a4d9930acc3142045315
Preetam Jinka <pj@isomero.us>
2014-02-01 08:43:10 -0500

    Add flow sample test

--- sflow_test.go
+++ sflow_test.go
@@ -11,4 +11,47 @@ func TestCounter(t *testing.T) {
 	if d.Header.SflowVersion != 5 {
 		t.Errorf("Expected datagram sFlow version to be %v, got %v", 5, d.Header.SflowVersion)
 	}
+	if len(d.Samples) != 1 {
+		t.Fatalf("Expected %v sample(s), got %v", 1, len(d.Samples))
+	}
+
+	cs := d.Samples[0].(CounterSample)
+	if cs.SampleType() != TypeCounterSample {
+		t.Fatalf("Expected a counter sample but didn't get one")
+	}
+
+	for _, record := range cs.Records {
+		if record.RecordType() == TypeGenericIfaceCounter {
+			i := record.(GenericIfaceCounters)
+			if i.Speed != 100000000 {
+				t.Errorf("Expected interface speed to be %v, got %v", 100000000, i.Speed)
+			}
+		}
+	}
+}
+
+func TestFlow(t *testing.T) {
+	packet, _ := ioutil.ReadFile("./_test/flow_sample.dump")
+	d := DecodeDatagram(packet)
+	if d.Header.SflowVersion != 5 {
+		t.Errorf("Expected datagram sFlow version to be %v, got %v", 5, d.Header.SflowVersion)
+	}
+	if len(d.Samples) != 1 {
+		t.Fatalf("Expected %v sample(s), got %v", 1, len(d.Samples))
+	}
+
+	cs := d.Samples[0].(FlowSample)
+	if cs.SampleType() != TypeFlowSample {
+		t.Fatalf("Expected a flow sample but didn't get one")
+	}
+
+	for _, record := range cs.Records {
+		if record.RecordType() == TypeExtendedSwitchFlow {
+			s := record.(ExtendedSwitchFlowRecord)
+			if !(s.DestinationVlan == s.SourceVlan && s.SourceVlan == 16) {
+				t.Errorf("Expected VLANs entries to be 16, got destination=%v, source=%v",
+					s.DestinationVlan, s.SourceVlan)
+			}
+		}
+	}
 }
